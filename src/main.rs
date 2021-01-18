3604d442ac788ae158d6a0d94360e18954ac3392
Bird <39280243+Birdthulu@users.noreply.github.com>
2021-01-17 19:30:17 -0500

    Get update path on mac

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![allow(non_snake_case)]
 mod utils;
 
+use std::path::Path;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    if std::env::consts::OS == ("macos") {
+        let arg0 = &std::env::args().next().unwrap();
+        let path = Path::new(arg0).parent().unwrap().join("../..");
+        println!("new path: {:?}", path);
+        std::env::set_current_dir(path);
+    }
+    println!("pwd: {:?}", std::env::current_dir());
     let download_information = utils::get_download_information().await;
     utils::parallel_download(download_information).await;
     let zip_file = utils::get_file().await;
